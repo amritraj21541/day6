aa40724728e51c7caffe0159f37f529474377392
amrit <amrit@MSI>
2022-06-15 14:19:25 +0530

    day6 done

---
+++ day6/src/day6/Demo1.java
@@ -0,0 +1,18 @@
+package day6;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i++)
+
+		{
+
+		System.out.println("Hello");
+
+		i++;
+
+		}
+
+	}
+
+}
